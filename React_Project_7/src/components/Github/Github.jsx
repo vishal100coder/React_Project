2d5143bd5e25ac915b585a562ba22d578fc30543
Vishal1879 <iamvishal.singh999@gmail.com>
2024-05-16 16:43:58 +0530

    React-router-Project

---
+++ React_Project_7/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+export default function Github() {
+  const data = useLoaderData();
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/vishal100coder")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     });
+  // }, []);
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  );
+}
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/vishal100coder");
+  return response.json();
+};
